d16be03339c4984278238ebb9aea3a4839578ccb
rasne1 <tizkfltm@naver.com>
2026-04-15 16:57:56 +0900

    remove submodule

---
+++ hello-spring2/src/main/java/com/ktdsuniversity/edu/members/dao/MembersDao.java
@@ -0,0 +1,35 @@
+package com.ktdsuniversity.edu.members.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ktdsuniversity.edu.members.vo.MembersVO;
+import com.ktdsuniversity.edu.members.vo.request.LoginVO;
+import com.ktdsuniversity.edu.members.vo.request.RegistVO;
+import com.ktdsuniversity.edu.members.vo.request.UpdateVO;
+
+@Mapper
+public interface MembersDao {
+
+	int insertNewMember(RegistVO registVO);
+
+	MembersVO selectMemberByEmail(String email);
+
+	int updateMemberByEmail(UpdateVO updateVO);
+
+	int deleteMemberByEmail(String email);
+
+	int selectMembersCount();
+
+	List<MembersVO> selectMembersList();
+
+	int updateIncreaseLoginFailCount(String email);
+
+	int updateBlock(String email);
+
+	int updateSuccessLogin(LoginVO loginVO);
+
+	List<String> selectMemberRolesByEmail(String username);
+
+}
---
+++ hello-spring2/src/main/java/com/ktdsuniversity/edu/replies/dao/RepliesDao.java
@@ -0,0 +1,28 @@
+package com.ktdsuniversity.edu.replies.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ktdsuniversity.edu.replies.vo.RepliesVO;
+import com.ktdsuniversity.edu.replies.vo.request.CreateVO;
+import com.ktdsuniversity.edu.replies.vo.request.UpdateVO;
+
+@Mapper
+public interface RepliesDao {
+
+	int insertNewReply(CreateVO createVO);
+
+	RepliesVO selectReplyByReplyId(String id);
+
+	int selectRepliesCountByArticleId(String articleId);
+
+	List<RepliesVO> selectRepliesByArticleId(String articleId);
+
+	int updateRecommendByReplyId(String replyId);
+
+	int deleteReplyByReplyId(String replyId);
+
+	int updateReplyByReplyId(UpdateVO updateVO);
+
+}
